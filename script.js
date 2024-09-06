7f9dab0c3822e08308f52a5081ec50af4c65db85
Animesh2392 <animeshgoliya@gmail.com>
2024-09-07 01:50:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// document.querySelectorAll('nav a').forEach(anchor => {
+//     anchor.addEventListener('click', function(e) {
+//         e.preventDefault();
+//         document.querySelector(this.getAttribute('href')).scrollIntoView({
+//             behavior: 'smooth'
+//         });
+//     });
+// });
+
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+    
+    console.log('Name:', name);
+    console.log('Email:', email);
+    console.log('Message:', message);
+    
+    alert('Thank you for your message!');
+    
+});
